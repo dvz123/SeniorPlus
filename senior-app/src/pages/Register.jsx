7ce6a49cbb8f3f0824282c59a30602db199527bc
Lucas Ramos Coelho <163207945+Lucas-Ramos-Coelho@users.noreply.github.com>
2025-05-21 19:19:38 -0300

    Update Register.jsx

--- senior-app/src/pages/Register.jsx
+++ senior-app/src/pages/Register.jsx
@@ -65,7 +65,7 @@ function Register() {
           >
             <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z" />
           </svg>
-          <h1 className="auth-logo-text">SENIOR+</h1>
+          <h1 className="auth-logo-text">Senior+</h1>
         </div>
 
         <h2 className="auth-title">Crie sua conta</h2>
